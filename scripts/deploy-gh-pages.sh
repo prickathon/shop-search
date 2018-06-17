c99d3aa88f3b3af460f460b2cb17ac1230a8a998
takanakahiko <takanakahiko@gmail.com>
2018-06-17 20:32:28 +0900

    CIを修正3

--- scripts/deploy-gh-pages.sh
+++ scripts/deploy-gh-pages.sh
@@ -29,12 +29,12 @@ git remote add --fetch origin "$remote"
 # switch into the the gh-pages branch
 if git rev-parse --verify origin/gh-pages-branch > /dev/null 2>&1
 then
-    git checkout gh-pages-branch
+    git checkout gh-pages
     # delete any old site as we are going to replace it
     # Note: this explodes if there aren't any, so moving it here for now
     git rm -rf .
 else
-    git checkout --orphan gh-pages-branch
+    git checkout --orphan gh-pages
 fi
 
 # copy over or recompile the new site
@@ -46,7 +46,7 @@ git add -A
 # now commit, ignoring branch gh-pages doesn't seem to work, so trying skip
 git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
 # and push, but send any output to /dev/null to hide anything sensitive
-git push --force --quiet origin gh-pages-branch > /dev/null 2>&1
+git push --force --quiet origin gh-pages > /dev/null 2>&1
 
 # go back to where we started and remove the gh-pages git repo we made and used
 # for deployment
